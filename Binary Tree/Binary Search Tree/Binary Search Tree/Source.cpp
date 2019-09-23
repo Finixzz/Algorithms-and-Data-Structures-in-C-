6873c58eb94fe99b2982a296fd38967de2b7e437
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-09-23 11:00:03 +0200

    Added function create binary search tree

---
+++ Binary Tree/Binary Search Tree/Binary Search Tree/Source.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+
+using namespace std;
+
+struct node {
+	node *left;
+	int key;
+	node *right;
+};
+class Tree {
+public:
+	node *root;
+
+	Tree():
+		root{nullptr}{}
+	~Tree() {
+		oslobodi_memoriju(root);
+	}
+	void oslobodi_memoriju(node *x) {
+		if (x) {
+			oslobodi_memoriju(x->left);
+			oslobodi_memoriju(x->right);
+			free(x);
+		}
+	}
+	void insert(int key) {
+		node *x = root;
+		node *t = nullptr;
+		if (!root) {
+			root = new node;
+			root->key = key;
+			root->left = root->right = nullptr;
+			return;
+		}
+		while (x) {
+			t = x;
+			if (key == x->key)
+				return;
+			else if (key < x->key)
+				x = x->left;
+			else
+				x = x->right;
+		}
+		node*p = new node;
+		p->key = key;
+		p->left = p->right = nullptr;
+		if (t->key > p->key)t->left = p;
+		else t->right = p;
+	}
+	void create_bst() {
+		int n, key;
+		cout << "Unesite broj vrhova stabla: "; cin >> n;
+		for (int i = 0; i < n; i++) {
+			cout << "Unesite vrijednost vrha: "; cin >> key;
+			insert(key);
+		}
+	}
+	void print_inorder(node *x) {
+		if (x) {
+			cout << x->key << " ";
+			print_inorder(x->left);
+			print_inorder(x->right);
+		}
+	}
+};
+
+int main() {
+
+	Tree t;
+	t.create_bst();
+	t.print_inorder(t.root);
+
+	system("pause");
+	return 0;
+}
