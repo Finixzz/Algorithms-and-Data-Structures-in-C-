bc8234a4707ef629954a089ce5bc6d2d274e3a3d
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-09-28 18:13:21 +0200

    Added functions count_height and balance_factor

--- Binary Tree/AVL trees/Project1/Project1/Source.cpp
+++ Binary Tree/AVL trees/Project1/Project1/Source.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
 struct node {
 	node *left;
 	int key;
+	int height;
 	node *right;
 };
 class Tree {
@@ -23,11 +24,24 @@ public:
 			free(x);
 		}
 	}
+	int count_height(node *x) {
+		if (x) {
+			int h = max(count_height(x->left), count_height(x->right))+1;
+			return h;
+		}
+		return 0;
+	}
+	int balance_factor(node *x) {
+		return count_height(x->left) - count_height(x->right);
+	}
 	void insert_node(int key) {
 		node *x = root;
 		node *t = nullptr;
+		node *p = new node;
+		p->height = 1;
 		if (!root) {
 			root = new node;
+			root->height = 1;
 			root->key = key;
 			root->left = root->right = nullptr;
 			return;
@@ -40,8 +54,8 @@ public:
 				x = x->left;
 			else
 				x = x->right;
+			p->height++;
 		}
-		node*p = new node;
 		p->key = key;
 		p->left = p->right = nullptr;
 		if (t->key > p->key)t->left = p;
@@ -56,16 +70,22 @@ public:
 			insert_node(key);
 		}
 	}
-	
-	
-	
+	void print_bf(node *x) {
+		if (x) {
+			print_bf(x->left);
+			cout << balance_factor(x) << " ";
+			print_bf(x->right);
+		}
+	}
 };
 
 int main() {
 
 	Tree t;
 	t.create_bst();
-	
+	cout << "Visina stabla: "<< t.count_height(t.root) << endl;
+	cout << "Balance faktor(preorder): "; t.print_bf(t.root);
+	cout << endl;
 	system("pause");
 	return 0;
 }
