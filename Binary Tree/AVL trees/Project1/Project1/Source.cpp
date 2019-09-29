8d7fe169a2ad88a47bb033b8f80c9d75b405bb35
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-09-29 19:56:44 +0200

    Refactored

--- Binary Tree/AVL trees/Project1/Project1/Source.cpp
+++ Binary Tree/AVL trees/Project1/Project1/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<algorithm>
+
 using namespace std;
 
 struct node {
@@ -11,8 +12,8 @@ class Tree {
 public:
 	node *root;
 
-	Tree() :
-		root{ nullptr } {}
+	Tree():
+		root{nullptr}{}
 	~Tree() {
 		oslobodi_memoriju(root);
 	}
@@ -23,15 +24,40 @@ public:
 			free(x);
 		}
 	}
+	
 	int count_height(node *x) {
 		if (x) {
-			int h = max(count_height(x->left), count_height(x->right))+1;
-			return h;
+			return max(count_height(x->left), count_height(x->right)) + 1;
 		}
 		return 0;
 	}
 	int balance_factor(node *x) {
-		return count_height(x->left) - count_height(x->right);
+		if (x) {
+			return count_height(x->left) - count_height(x->right);
+		}
+		return 0;
+	}
+
+	void print_bf(node *x) {
+		if (x) {
+			print_bf(x->left);
+			cout << balance_factor(x) << " ";
+			print_bf(x->right);
+		}
+	}
+	node *LLrotation(node *x) {
+		node *t = x->left;
+		x->left = t->right;
+		t->right = x;
+		root = t;
+		return t;
+	}
+	node *RRrotation(node *x) {
+		node *t = x->right;
+		x->right = t->left;
+		t->left = x;
+		root = t;
+		return t;
 	}
 	node * insert_node(node *x,int key) {
 		if (x == nullptr) {
@@ -41,27 +67,21 @@ public:
 			return x;
 		}
 		if (key == x->key)
-			return NULL;
-		else if (key < x->key) {
-			x->left = insert_node(x->left, key);
+			return 0;
+		else if (key < x->key)
+			x->left=insert_node(x->left,key);
+		else
+			x->right=insert_node(x->right, key);
+
+		if (balance_factor(x) == 2 && balance_factor(x->left) == 1) {
+			return LLrotation(x);
 		}
-		else {
-			x->right = insert_node(x->right, key);
+		if (balance_factor(x) == -2 && balance_factor(x->right) == -1) {
+			return RRrotation(x);
 		}
 		return x;
+	}
 
-	}
-	
-	void LLrotation(node *x) {
-		if (balance_factor(x) == 2 && balance_factor(x->left) == 1) {
-			node *t;
-			t = x->left;
-			x->left = t->right;
-			t->right = x;
-			root = t;
-			
-		}
-	}
 	void print_inorder(node *x) {
 		if (x) {
 			print_inorder(x->left);
@@ -69,57 +89,22 @@ public:
 			print_inorder(x->right);
 		}
 	}
-	void LLrotate(node *x) {
-		if (x) {
-			LLrotation(x);
-			LLrotate(x->left);
-			LLrotate(x->right);
-		}
-	}
-
-	void RRrotation(node *x) {
-		if (balance_factor(x) == 2 && balance_factor(x->right)) {
-			node *t = x->right;
-			x->right = t->left;
-			t->left = x;
-			root = t;
-		}
-	}
-	void RRrotate(node *x) {
-		if (x) {
-			RRrotation(x);
-			RRrotate(x->left);
-			RRrotate(x->right);
-		}
-	}
-	void print_bf(node *x) {
-		if (x) {
-			print_bf(x->left);
-			cout << balance_factor(x) << " , ";
-			print_bf(x->right);
-		}
-	}
 };
+
 
 int main() {
 
+
 	Tree t;
 	int key = 0;
-	cin >> key;
+	cout << "Unesite root vrijednost: "; cin >> key;
 	t.root = t.insert_node(t.root, key);
 	while (key != -1) {
-		cout << "cin: "; cin >> key;
+		cout << "Unesite vrijednost vrha: "; cin >> key;
 		if (key == -1)break;
-		t.insert_node(t.root,key);
-		
+		t.insert_node(t.root, key);
 	}
-	t.LLrotate(t.root);
-	//t.RRrotate(t.root);
-	t.print_inorder(t.root); cout << endl;
-	cout << "Visina stabla: "<< t.count_height(t.root) << endl;
-	 t.print_bf(t.root);
-	//cout << "Balance faktor(preorder): "; t.print_bf(t.root);
-	cout << endl;
+	cout << "Preorder: "; t.print_inorder(t.root); cout << endl;
 	system("pause");
 	return 0;
 }
