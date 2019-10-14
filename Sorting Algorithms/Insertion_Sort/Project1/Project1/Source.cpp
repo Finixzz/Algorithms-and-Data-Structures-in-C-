20015ec7156535d471e1f5c90a597688060e1a4e
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-10-14 09:01:22 +0200

    Finished!

--- Sorting Algorithms/Insertion_Sort/Project1/Project1/Source.cpp
+++ Sorting Algorithms/Insertion_Sort/Project1/Project1/Source.cpp
@@ -2,10 +2,39 @@
 
 using namespace std;
 
+void set(int *A, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "Unesite " << i + 1 << ". clan niza: "; cin >> A[i];
+	}
+}
+void print(int *A, int n) {
+	cout << "Clanovi niza su: ";
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+}
+void selection_sort(int *A, int n) {
+	int temp, j;
+	for (int i = 1; i < n; i++) {
+		temp = A[i];
+		j = i - 1;
+		while (j > -1 && A[j] > temp) {
+			A[j + 1] = A[j];
+			j--;
+		}
+		A[j + 1] = temp;
+	}
+}
 int main() {
 
+	int n;
+	cout << "Unesite broj clanova niza: "; cin >> n;
+	int *A = new int[n];
+	set(A, n);
+	selection_sort(A, n);
+	print(A, n);
 
-
+	delete[]A; A = nullptr;
 	system("pause");
 	return 0;
 }
