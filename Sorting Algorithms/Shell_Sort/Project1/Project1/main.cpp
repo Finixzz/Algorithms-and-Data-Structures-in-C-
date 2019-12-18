c3fc1e7102a3ff4ffbab172ba986a03171644b18
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-12-18 20:15:04 +0100

    Finished

--- Sorting Algorithms/Shell_Sort/Project1/Project1/main.cpp
+++ Sorting Algorithms/Shell_Sort/Project1/Project1/main.cpp
@@ -2,11 +2,44 @@
 
 using namespace std;
 
+void set(int *A, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "Unesite " << i + 1 << ". clan niza: "; cin >> A[i];
+	}
+}
+
+void print(int *A, int n) {
+	cout << "Clanovi niza su: ";
+	for (int i = 0; i < n; i++) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
+void shell_sort(int *A, int n) {
+	int temp, j;
+	for (int gap = n / 2; gap >= 1; gap /= 2) {
+		for (int i = gap; i < n; i++) {
+			temp = A[i];
+			j = i - gap;
+			while (j >= 0 && A[j] > temp) {
+				A[j + gap] = A[j];
+				j = j - gap;
+			}
+			A[j + gap] = temp;
+		}
+	}
+}
+
 int main() {
 
-
-
-
-	system("Pause");
+	int n;
+	cout << "Unesite broj clanova niza: "; cin >> n;
+	int *A = new int[n];
+	set(A, n);
+	shell_sort(A, n);
+	print(A, n);
+	delete[]A; A = nullptr;
+	system("pause");
 	return 0;
 }
